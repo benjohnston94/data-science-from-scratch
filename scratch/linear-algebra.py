1641f29cdb51fcf63c4af1e8763df6497636cdec
Ben <ben@ben-laptop.fritz.box>
2020-11-07 16:44:10 +1300

    add more functions to linear algebra: subtract, scalar multiply, vector mean, vector sum

--- scratch/linear-algebra.py
+++ scratch/linear-algebra.py
@@ -4,28 +4,91 @@ Directory:
 - Add two vectors
 - Subtract two vectors
 - Vector sum (component-wise of multiple vectors)
-- vector mean
 - Scalar multiply
+- vector mean
 - Dot
 - sum of sqaures
 - magnitude
 - distance
 """
+from typing import List
 
-"""
-A Vector is just a one dimensional array
-"""
-from numpy import array as Vector
+# these should later be turned into classes
+Vector = List[float]  # A Vector is a one dimensional array
+Matrix = List[Vector]  # A Matrix is list of vectors
+
 
 def add(v: Vector, w: Vector) -> Vector:
     """
     Add the components of two vectors 
-    The i dimensions will add together
     Vectors must be the same length
     """
     assert len(v) == len(w), "Vectors must be the same length!"
 
     return [v[i] + w[i] for i in range(len(v))]
 
-assert add([1, 2, 3], [2, 4, 6]) == [3, 6, 9]
 
+assert add([1, 2, 3], [2, 4, 6]) == [3, 6, 9]
+assert not add([1, 2, 3], [1, 2, 3]) == [1, 1, 1]
+
+
+def subtract(v: Vector, w: Vector) -> Vector:
+    """
+    Subtract the components of two vectors
+    vectors must be the same length
+    """
+    assert len(v) == len(w), "Vectors must be the same length!"
+
+    return [v[i] - w[i] for i in range(len(v))]
+
+
+assert subtract([1, 2, 3], [2, 4, 6]) == [-1, -2, -3]
+
+
+def vector_sum(matrix: Matrix) -> Vector:
+    """
+    Takes in a matrix (2d array of vectors)
+    Adds up elements of all vectors component-wise
+    All vectors must be the same length
+    """
+    assert matrix, "Matrix must not be empty!"
+    n_dims = len(matrix[0])
+    assert all(len(v) == n_dims for v in matrix), "all vectors must be the same length"
+
+    return [sum(v) for v in zip(*matrix)]
+
+
+assert vector_sum([[1, 1, 1],
+                   [1, 2, 3],
+                   [2, 2, 2]]) == [4, 5, 6]
+
+
+def scalar_multiply(v: Vector, x: int) -> Vector:
+    """
+    Multiplies each component of a vector by
+    a constant x
+    """
+    return [v[i] * x for i in range(len(v))]
+
+
+assert scalar_multiply([1, 2, 3], 2) == [2, 4, 6]
+assert not scalar_multiply([1, 2, 3], 2) == [1, 2, 3]
+
+
+def vector_mean(matrix: Matrix) -> Vector:
+    """
+    Takes in a list of n-dimensional vectors
+    Returns a vector with the component-wise mean of input vectors
+    """
+    assert matrix, "Matrix must not be empty!"
+    m_len = len(matrix)
+    # assert all(len(v) == n_dims for v in matrix), "all vectors must be the same length"
+
+    v_summed = vector_sum(matrix)
+
+    return scalar_multiply(v_summed, 1 / m_len)
+
+
+assert vector_mean([[1, 2, 3],
+                    [2, 3, 4],
+                    [3, 4, 8]]) == [2, 3, 5]
